9f337583aaf094d93ee8363d11de08afe5db22a2
Samsung <elenakakovkina89@gmail.com>
2024-07-03 22:28:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int guests = 4;
+int guestsPerTable = 2;
+int currentGuest = 0;
+
+while (currentGuest < guests)
+{
+    for (int i = 0; i < guestsPerTable && currentGuest < guests; i++)
+    {
+        Console.WriteLine("Hi! What is your name?");
+        string UserName = Console.ReadLine();
+
+        Console.WriteLine($"Hi {UserName}. Your table #{(currentGuest / guestsPerTable) + 1}");
+        currentGuest++;
+
+    }
+
+    Console.WriteLine($"Table #{(currentGuest / guestsPerTable)} is now full.");
+}
+
+Console.WriteLine("No free places");
